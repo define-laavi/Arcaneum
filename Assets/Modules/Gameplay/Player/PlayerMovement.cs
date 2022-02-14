12dafe46f08a183c5749e21ce3110d16fc13b0a3
Mateusz Gawlik-Kałużny <66747844+selavidll@users.noreply.github.com>
2022-02-14 14:48:06 +0100

    Crossing edges
    Created basic scene ("Gameplay")
    Added World script responsible for world looping.
    Added basic player script allowing for movement with arrow keys.

---
+++ Assets/Modules/Gameplay/Player/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Asteroids.Modules.Gameplay;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+    
+    void Update()
+    {
+        var position = transform.position;
+        position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed;
+        position = World.PlaceInPlayArea(position);
+        transform.position = position;
+    }
+}
---
+++ Assets/Modules/Gameplay/World.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+namespace Asteroids.Modules.Gameplay
+{
+    public class World : MonoBehaviour
+    {
+        private static World _instance;
+    
+        public Camera gameCamera;
+
+        private static Vector2 _gameWorldMinimum, _gameWorldMaximum;
+
+        private static float Width => _gameWorldMaximum.x - _gameWorldMinimum.x;
+        private static float Height => _gameWorldMaximum.y - _gameWorldMinimum.y;
+        
+        void Start()
+        {
+            if (_instance != null)
+                Destroy(this); //More than one world can't exist at the same time.
+        
+            if (gameCamera == null)
+                throw new Exception("World Camera is null. Please add the reference to world script!");
+            
+            _instance = this;
+
+            _gameWorldMinimum = gameCamera.ScreenToWorldPoint(Vector2.zero);
+            _gameWorldMaximum = gameCamera.ScreenToWorldPoint(gameCamera.pixelRect.size);
+            
+            Debug.Log(PlaceInPlayArea(_gameWorldMinimum - Vector2.one));
+            Debug.Log(PlaceInPlayArea(_gameWorldMaximum));
+        }
+    
+        public static bool IsInPlayArea(Vector2 point)
+        {
+            if (_instance == null)
+                throw new Exception("There is no world in the game scene!");
+
+            return point.x >= _gameWorldMinimum.x && point.x <= _gameWorldMaximum.x &&
+                   point.y >= _gameWorldMinimum.y && point.y <= _gameWorldMaximum.y;
+        }
+
+        public static Vector2 PlaceInPlayArea(Vector2 point)
+        {
+            if (IsInPlayArea(point)) return point;
+
+            var shifted = point - _gameWorldMinimum;
+
+            return new Vector2((shifted.x % Width + Width) % Width, (shifted.y % Height + Height) % Height) + _gameWorldMinimum;
+        }
+    }
+}
+
